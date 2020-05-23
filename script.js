b547e1a0aa6c4fc2915e918c5d6a1f8c96cd99e2
Rohan <nayakwadigagan42@gmail.com>
2020-05-23 04:01:44 -0500

    update upto 05.23.2020

---
+++ script.js
@@ -0,0 +1,16 @@
+// const lTetromino ='firstShape'
+// const zTetromino ='secondShape'
+// const oTetromino ='thirdShape'
+// const iTetromino ='fourthShape'
+// const tTetromino ='fifthShape'
+//
+// const tetrominoes = [lTetromino,zTetromino, oTetromino,iTetromino,tTetromino]
+// console.log(tetrominoes[4])
+
+
+
+let names =['Gagan','Rohan','Bob','Nittu']
+
+names.forEach(name => {
+  console.log(name + ' is thopu')
+} )
